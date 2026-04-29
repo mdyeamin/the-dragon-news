269bffad8058e1f73e298e94f645281714b5cfb5
mdyeamin <ahmedyeamin05@gmail.com>
2026-04-29 14:06:00 +0600

    baseURL from env

--- src/proxy.js
+++ src/proxy.js
@@ -1,10 +1,17 @@
 import { NextResponse } from "next/server";
-
-export function proxy(request) {
-  return NextResponse.redirect(new URL('/', request.url))
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) {
+    NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 }
-
 
 export const config = {
-  matcher: '/career',
-}
+  matcher: ["/career","/news:id*"],
+};
